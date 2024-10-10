cd8f4f7b6a438bcda6c03392ef7eb8d9d303add0
Christian Morrell <cmorrell@unb.ca>
2024-10-10 17:28:37 -0300

    Combine stream and validate scripts into one

--- stream.py
+++ stream.py
@@ -2,73 +2,94 @@ import argparse
 from pathlib import Path
 
 import libemg
+from libemg.data_handler import RegexFilter, FilePackager
 import numpy as np
 
-from utils.data_collection import collect_data, Device
+from config import Config
 
 # -> python stream.py sifi collect Data/subject1000/ images/ [abduct-adduct-flexion-extension] 5
 def main():
     # CLI arguments
-    parser = argparse.ArgumentParser(description='Stream EMG data for visualization or collection.')
+    parser = argparse.ArgumentParser(description='Stream EMG data for visualization or collection.', usage='python main.py sifi data/subject1000 sgt images')
     parser.add_argument('device', type=str, choices=('emager', 'myo', 'oymotion', 'sifi'), help='Device to stream. Choices are emager, myo, oymotion, sifi.')
-    parser.add_argument('--skip_analyze', action='store_true', help='Flag to skip calling analyze_hardware() method.')
+    parser.add_argument('subject_id', type=str, help='Subject ID.')
+    parser.add_argument('model', type=str, choices=('within-sgt', 'combined-sgt', 'within-ciil', 'combined-ciil'), help='Model type. Choices are within-sgt, combined-sgt, within-ciil, and combined-ciil.')
+    parser.add_argument('--analyze', action='store_true', help='Flag to call analyze_hardware() method.')
     subparsers = parser.add_subparsers(description='Streaming objectives.', dest='objective', required=True)
 
-    visualize_parser = subparsers.add_parser('visualize', description='Live visualization of EMG signal.')
-    visualize_parser.add_argument('--num_samples', type=int, default=1000, help='Number of samples to use per plot frame.')
-    visualize_subparsers = visualize_parser.add_subparsers(description='Visualization methods.', dest='method', required=True)
-    visualize_subparsers.add_parser('heatmap', description='Spatial heatmap visualization.')
-    visualize_subparsers.add_parser('time', description='Time series plot with all channels on one plot.')
-    channels_parser = visualize_subparsers.add_parser('channels', description='Time series plot with each channels on its own plot.')
-    channels_parser.add_argument('channels', type=str, help="Channels to display. Pass as a string of comma-separated values (e.g., '1,2,10').")
+    sgt_parser = subparsers.add_parser('sgt', description='Collect data.')
+    sgt_parser.add_argument('media_directory', type=str, help='Relative path to animation directory (minus movements).')
+    sgt_parser.add_argument('--visualization_method', default='time', type=str, help='Visualization method before collecting data. Options are heatmap, time, or comma-separated channels (e.g., 4,8,10).')
 
-    collect_parser = subparsers.add_parser('collect', description='Collect data.')
-    collect_parser.add_argument('data_directory', type=str, help='Relative path to data directory (minus movements). Collected data will be stored in data_directory.')
-    collect_parser.add_argument('media_directory', type=str, help='Relative path to animation directory (minus movements).')
-    collect_parser.add_argument('movements', type=str, help='Motion combinations to perform. List separated by commas. Options are open-close, pro-sup, open-pro-close-sup, close-pro-open-sup.')
-    collect_parser.add_argument('num_reps', type=int, help='# of reps.')
+    fitts_parser = subparsers.add_parser('fitts', description='Perform Fitts task.')
+    fitts_parser.add_argument('--feature_set', type=str, default='MSWT', choices=libemg.feature_extractor.FeatureExtractor.get_feature_groups().keys(), help='Feature set.')
+
+    # Thinking of moving this to a main.py file instead of separate ones
+    # Have two stages: sgt and fitts
+    # If it's sgt then we visualize automatically
+    # Use config methods (e.g., setup_live_processing)
     
     args = parser.parse_args()
     print(args)
 
+    config = Config(subject_id=args.subject_id, model=args.model, stage='sgt')
 
-    device = Device(args.device)
-    fs = device.fs
-    smi = device.stream()
-
-    online_data_handler = libemg.data_handler.OnlineDataHandler(shared_memory_items=smi)
-    if not args.skip_analyze:
+    online_data_handler = config.setup_live_processing()
+    if args.analyze:
         online_data_handler.analyze_hardware()
     
-    if args.objective == 'visualize':
-        fi = libemg.filtering.Filter(fs)
-        fi.install_common_filters()
-        online_data_handler.install_filter(fi)
-
+    if args.objective == 'sgt':
         # Visualize
-        method = args.method
+        method = args.visualization_method
         if method == 'heatmap':
             remap_function = lambda x: np.reshape(x, (x.shape[0], 4, 16))
             online_data_handler.visualize_heatmap(num_samples=args.num_samples, remap_function=remap_function, feature_list=['MAV', 'RMS'])
         elif method == 'time':
             online_data_handler.visualize(num_samples=args.num_samples, block=True)
-        elif method == 'channels':
-            channels = args.channels.split(',')
+        else:
+            # Passed in list of channels
+            channels = method.replace(' ', '').split(',')
             channels = list(map(int, channels))
             online_data_handler.visualize_channels(channels, num_samples=args.num_samples)
-        else:
-            raise ValueError(f"Unexpected value for visualization method. Got: {method}.")
-    elif args.objective == 'collect':
-        # Collect data
-        movements = args.movements.replace(' ', '') # remove any spaces
-        movements = movements.split(',')
-        for movement in movements:
-            media_directory = Path(args.media_directory, movement).absolute().as_posix()
-            data_directory = Path(args.data_directory, movement).absolute().as_posix()
 
-            collect_data(online_data_handler, media_directory, data_directory, args.num_reps)
+        config.start_sgt()
 
-    print('Stream finished.')
+    elif args.objective == 'fitts':
+        # Add filter
+        fi = libemg.filtering.Filter(config.device.fs)
+        fi.install_common_filters()
+        online_data_handler.install_filter(fi)
+
+        # Parse data
+        odh = libemg.data_handler.OfflineDataHandler()
+        regex_filters = (
+            RegexFilter(left_bound='/', right_bound='/', values=[args.subject_id], description='subject'),
+            RegexFilter(left_bound='C_0_R_', right_bound='_emg.csv', values=[str(idx) for idx in range(config.DC_reps)], description='reps')
+        )
+        package_function = lambda x, y: Path(x).parent.absolute().as_posix() == Path(y).parent.absolute().as_posix()
+        metadata_fetchers = (
+            FilePackager(RegexFilter('/', '.txt', values=['collection'], description='labels'), package_function),
+        )
+        data_directory = Path(args.data_directory).absolute().as_posix()
+        odh.get_data(data_directory, regex_filters, metadata_fetchers)
+
+        windows, metadata = odh.parse_windows(config.window_length, config.window_increment, metadata_operations=dict(labels=lambda x: x[-1]))
+        fe = libemg.feature_extractor.FeatureExtractor()
+        feature_list = fe.get_feature_groups()['HTD']
+        features = fe.extract_features(feature_list, windows, array=True)
+        labels = metadata['labels']
+
+        # Model setup
+        config.prepare_model_from_sgt() # fits model and stores as .pkl
+        model = config.setup_model()
+
+        # Create Fitts environment
+        controller = libemg.environments.controllers.RegressorController()
+        isofitts = libemg.environments.isofitts.IsoFitts(controller, num_circles=8, num_trials=20, dwell_time=1.0,
+                                                         save_file=Path(config.DC_model_file).with_name('fitts.pkl').as_posix())
+        isofitts.run()
+
+    print('------------------Main script complete------------------')
 
 
 if __name__ == '__main__':
