c2453ae1a01fbb15c6097dc9ef1b9f072bea6359
Christian Morrell <cmorrell@unb.ca>
2024-10-16 16:13:35 -0300

    Remove experience_ids from Memory

--- utils/adaptation.py
+++ utils/adaptation.py
@@ -37,8 +37,6 @@ class Memory:
         self.experience_context    = []
         # What was the outcome (P or N)
         self.experience_outcome    = []
-        # What is the id of the experience
-        self.experience_ids        = []
         # How many memories do we have?
         self.experience_timestamps = []
         self.memories_stored = 0
@@ -55,13 +53,11 @@ class Memory:
                 self.experience_context    = other_memory.experience_context
                 self.experience_ids        = other_memory.experience_ids
                 self.experience_outcome    = other_memory.experience_outcome
-                self.experience_timestamps = other_memory.experience_timestamps
                 self.memories_stored       = other_memory.memories_stored
             else:
                 self.experience_targets = torch.cat((self.experience_targets,other_memory.experience_targets))
                 self.experience_data = torch.vstack((self.experience_data,other_memory.experience_data))
                 self.experience_context = np.concatenate((self.experience_context,other_memory.experience_context))
-                self.experience_ids.extend(list(range(self.memories_stored, self.memories_stored + other_memory.memories_stored)))
                 self.experience_outcome = np.concatenate((self.experience_outcome, other_memory.experience_outcome)) 
                 self.experience_timestamps.extend(other_memory.experience_timestamps)
                 self.memories_stored += other_memory.memories_stored
@@ -73,7 +69,6 @@ class Memory:
                 self.experience_targets = experience_targets
                 self.experience_data    = experience_data
                 self.experience_context = experience_context
-                self.experience_ids     = list(range(len(experience_targets)))
                 self.experience_outcome = experience_outcome
                 self.experience_timestamps = experience_timestamps
                 self.memories_stored    += len(experience_targets)
@@ -81,7 +76,6 @@ class Memory:
                 self.experience_targets = torch.cat((self.experience_targets,experience_targets))
                 self.experience_data = torch.vstack((self.experience_data,experience_data))
                 self.experience_context = np.concatenate((self.experience_context,experience_context))
-                self.experience_ids.extend(list(range(self.memories_stored, self.memories_stored + len(experience_targets))))
                 self.experience_outcome = np.concatenate((self.experience_outcome, experience_outcome)) 
                 self.experience_timestamps.extend(experience_timestamps)
                 self.memories_stored += len(experience_targets)
@@ -93,7 +87,6 @@ class Memory:
             # shuffle the keys
             self.experience_targets = self.experience_targets[indices]
             self.experience_data    = self.experience_data[indices]
-            self.experience_ids     = [self.experience_ids[i] for i in indices]
             # SGT does not have these fields
             if len(self.experience_context):
                 self.experience_context = self.experience_context[indices]
@@ -102,7 +95,8 @@ class Memory:
 
         
     def unshuffle(self):
-        unshuffle_ids = [i[0] for i in sorted(enumerate(self.experience_ids), key=lambda x:x[1])]
+        # TODO: Test that this works using timestamp instead of IDs
+        unshuffle_ids = [i[0] for i in sorted(enumerate(self.experience_timestamps), key=lambda x:x[1])]
         if len(self):
             self.experience_targets = self.experience_targets[unshuffle_ids]
             self.experience_data    = self.experience_data[unshuffle_ids]
@@ -110,7 +104,6 @@ class Memory:
             if len(self.experience_context):
                 self.experience_context = self.experience_context[unshuffle_ids]
                 self.experience_outcome = [self.experience_outcome[i] for i in unshuffle_ids]
-                self.experience_ids     = [self.experience_ids[i] for i in unshuffle_ids]
                 self.experience_timestamps = [self.experience_timestamps[i] for i in unshuffle_ids]
 
     def write(self, save_dir, num_written=""):
@@ -124,7 +117,6 @@ class Memory:
             self.experience_data    = loaded_content.experience_data
             self.experience_context = loaded_content.experience_context
             self.experience_outcome = loaded_content.experience_outcome
-            self.experience_ids     = loaded_content.experience_ids
             self.memories_stored    = loaded_content.memories_stored
             self.experience_timestamps = loaded_content.experience_timestamps
     
@@ -135,7 +127,6 @@ class Memory:
         self.experience_data    = obj.experience_data
         self.experience_context = obj.experience_context
         self.experience_outcome = obj.experience_outcome
-        self.experience_ids     = obj.experience_ids
         self.memories_stored    = obj.memories_stored
         self.experience_timestamps = obj.experience_timestamps
 
